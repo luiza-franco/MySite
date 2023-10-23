a24b50b63257f2c2d8609702124a03d6f994e76c
Luiza Bade Franco <luizarbfranco@gmail.com>
2023-10-22 23:52:51 -0300

    import completed

--- import_data.py
+++ import_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from polls.models import Question
+from polls.models import Choice
 import os
 import django
+import numpy as np
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
@@ -13,7 +15,23 @@ def import_data(file_path):
         question = Question.objects.create(
             question_text=row['Question']
         )
+        for i in range(1, len(df.columns) // 2 + 1): 
+            choice_col = f'Choice {i}'
+            votes_col = f'Votes {i}'
 
-file_path = r'C:\Users\luiza\Documents\mysite\polls\dados\dados.xlsx'
-import_data(file_path)
+            answer_text = row[choice_col]
+            votes = row[votes_col]
+            if np.isnan(votes):
+                votes = 0 #quando for nulo, gera 0
 
+
+            choice = Choice.objects.create( #pega os objetos de models 
+                question=question,
+                choice_text=answer_text,
+                votes=votes
+            )
+
+
+
+full_path = r"C:\Users\luiza\Documents\mysite\polls\dados\dados.xlsx"
+import_data(full_path)
